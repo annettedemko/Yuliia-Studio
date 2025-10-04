d28e1ce89eed4694f9dd22b491a7f09468dc2a94
Hanna Demko <annettedemko@gmail.com>
2025-10-04 12:16:02 +0200

    fix: add missing closing div tag

--- src/pages/Physiq360.tsx
+++ src/pages/Physiq360.tsx
@@ -335,6 +335,7 @@ const Physiq360 = () => {
                 <div className="absolute inset-0 bg-gradient-to-t from-black/30 to-transparent rounded-xl opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
               </div>
             </div>
+            </div>
           </div>
         </div>
       </section>
